7ce87a9ec2c773f721d9902ebfb18afaccdee24f
RehamSobhy9 <rehamsobhy819@gmail.com>
2025-06-21 10:55:29 +0300

    new updates on weather app

--- lib/views/user_location.dart
+++ lib/views/user_location.dart
@@ -18,6 +18,24 @@ class _user_locationState extends State<user_location> {
   ApiState apistate = ApiState.none;
   Weatherdata model = Weatherdata();
 
+  String getweatherimage(int id){
+      if(id <=32){
+        return 'images/thunder.png';
+      } else if(id <= 321){
+        return 'images/drizzle.png';
+      }else if(id <= 531){
+        return 'images/rain.png';
+      }else if(id <= 622){
+        return 'images/snow.png';
+      }else if(id <= 781){
+        return 'images/tornado.png';
+      }else if(id == 800){
+        return 'images/clear.png';
+      }else{
+        return 'images/cloudy.png';
+      }
+  }
+
   getweatherdata() async {
     setState(() {
       apistate = ApiState.loading;
@@ -53,22 +71,126 @@ class _user_locationState extends State<user_location> {
   @override
   Widget build(BuildContext context) {
     return apistate == ApiState.loading
-        ? Scaffold(body: Center(child: CircularProgressIndicator()))
+        ? Scaffold(
+            backgroundColor: Color(0xff3A3F54),
+            body: Center(
+              child: CircularProgressIndicator(color: Colors.white60),
+            ),
+          )
         : apistate == ApiState.error
         ? Scaffold(
+            backgroundColor: Color(0xff3A3F54),
             body: Center(
-              child: Text("There is error", style: TextStyle(fontSize: 50)),
+              child: Text(
+                "Error",
+                style: TextStyle(fontSize: 60, color: Colors.white60),
+              ),
             ),
           )
         : Scaffold(
-            appBar: AppBar(title: Text("User Location"), centerTitle: true),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(model.name!, style: TextStyle(fontSize: 60)),
-                Text(model.sys!.country!, style: TextStyle(fontSize: 60)),
-              ],
+            backgroundColor: Color(0xff3A3F54),
+            appBar: AppBar(
+              title: Text(
+                "User Location",
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              centerTitle: true,
+              backgroundColor: Color(0xff3A3F54),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    model.name!,
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "${(model.main!.temp! - 273.15).toStringAsFixed(1)} °",
+                        style: TextStyle(
+                          fontSize: 60,
+                          color: Colors.white60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Image(
+                        color: Colors.white60,
+                          height: 150,
+                          width: 150,
+                          image: AssetImage(
+                        getweatherimage(model.weather![0].id!)
+                      ))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    "${model.weather![0].main} in ${model.sys!.country!}",
+                    style: TextStyle(fontSize: 30, color: Colors.white60,fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    "Lat : ${model.coord!.lat!} -Lon : ${model.coord!.lon!}",
+                    style: TextStyle(fontSize: 30, color: Colors.white60,fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text("Wind Speed",style: TextStyle(fontSize: 25, color: Colors.white60,fontWeight: FontWeight.bold),),
+                          Text(
+                            (model.wind!.speed!).toString(),
+                            style: TextStyle(fontSize: 25, color: Colors.white60,fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("visibility",style: TextStyle(fontSize: 25, color: Colors.white60,fontWeight: FontWeight.bold),),
+                          Text(
+                            (model.visibility!).toString(),
+                            style: TextStyle(fontSize: 25, color: Colors.white60,fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("humidity",style: TextStyle(fontSize: 25, color: Colors.white60,fontWeight: FontWeight.bold),),
+                          Text(
+                            (model.main!.humidity!).toString(),
+                            style: TextStyle(fontSize: 25, color: Colors.white60,fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
             ),
           );
   }
